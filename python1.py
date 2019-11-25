e548e3e5a2b5e1aaa285252f05ebcfeb404e321d
ariahendrawan713 <38807873+ariahendrawan713@users.noreply.github.com>
2019-11-25 16:55:53 +0700

    membuat latihan script python
    latihan python untuk mahasiswa

---
+++ python1.py
@@ -0,0 +1,27 @@
+a = 5
+b = 3
+
+# penjumlahan
+c = a + b
+print("hasil a + b = ", c)
+
+# pengurangan
+c = a - b
+print("hasil a - b = ", c)
+
+# perkalian
+c = a*b
+print("hasil a*b = ", c)
+
+# pembagian
+c = a / b
+print("hasil a/b =", c)
+
+# pembagian pembulatan keatas
+# silakan diisi bagaimana formulanya
+
+# modulus
+# silakan diisi bagaimana formulanya
+
+# pangkat
+# silakan diisi bagaimana formulanya
