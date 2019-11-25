1071312d7e0cb83d3dee097a91445a7e8317e45e
ariahendrawan713 <38807873+ariahendrawan713@users.noreply.github.com>
2019-11-25 17:00:45 +0700

    membuat file python2
    latihan mahasiswa untuk membuat python lalu di push di repository ini

---
+++ python2.py
@@ -0,0 +1,25 @@
+a = 5
+b = 3
+c = 4.0
+
+# penjumlahan
+
+print("penjumlahan a + b = ", a + b)
+
+# pengurangan
+
+print("pengurangan a - b = ", a - b)
+
+# perkalian
+
+print("perkalian a * b = ", a * b)
+
+# pembagian
+
+print("pembagian a / b = ", a / b)
+
+# modulus
+# silakan diisi bagaimana formulanya dengan a modulus b; b modulus c
+
+# pangkat
+# silakan diisi bagaimana formulanya dengan a pangkat b; b pangkat c
