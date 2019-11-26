8502f9cfb40a2563aaf613d4f341dd3b6d06ea0d
yufafx <yusufpra91@gmail.com>
2019-11-26 15:02:29 +0700

    Nambah Script Python

--- python2.py
+++ python2.py
@@ -20,6 +20,10 @@ print("pembagian a / b = ", a / b)
 
 # modulus
 # silakan diisi bagaimana formulanya dengan a modulus b; b modulus c
+print("modulus a % b = ", a % b)
+print("modulus b % c = ", b % c)
 
 # pangkat
 # silakan diisi bagaimana formulanya dengan a pangkat b; b pangkat c
+print("pangkat a ^ b = ", a ** b)
+print("pangkat b ^ c = ", b ** c)
