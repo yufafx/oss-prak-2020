097596b8b9f23acb311c43307fbee20fe43429e3
yufafx <yusufpra91@gmail.com>
2019-11-26 15:02:11 +0700

    Responsi Praktek OSS

--- python1.py
+++ python1.py
@@ -19,9 +19,18 @@ print("hasil a/b =", c)
 
 # pembagian pembulatan keatas
 # silakan diisi bagaimana formulanya
+c = round(a / b)
+print("hasil pembulatan a/b =", c)
+
 
 # modulus
 # silakan diisi bagaimana formulanya
+c = a%b
+print("hasil a%b =", c)
 
 # pangkat
 # silakan diisi bagaimana formulanya
+c = (a*a)
+print("hasil pangkat cara 1 : a*a =", c)
+c = (a**2)
+print("hasil pangkat cara 2 : a**2 =", c)
